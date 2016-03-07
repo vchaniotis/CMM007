3aa39d2bfd6e2d0568875f8fc70421a7fb7d1829
A <A>
2016-03-07 09:55:46 +0000

    7-3-2016

--- 07-3/index.php
+++ 07-3/index.php
@@ -6,6 +6,13 @@
     <p>
         <?php
         echo "Hello World";
+        echo "Hello," . " " . "world" . "!";
+        echo 5 * 7;
+        $myname = "Frodo Baggins";
+        $myage = 111;
+        echo "My name is " . $myname . " and I am " . $myage;
+
+        $name = "Edgar";
         ?>
     </p>
 </body>
