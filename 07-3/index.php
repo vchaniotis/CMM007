97b3f5b84dd1b1fe15a2bf5f9ad6109188c3f772
A <A>
2016-03-07 10:01:57 +0000

    7-3-2016

--- 07-3/index.php
+++ 07-3/index.php
@@ -21,7 +21,18 @@
             print "Who are you?";
         }
 
+        $myname = "Vasilis";
+        $myage = 11;
 
+        if ($myage > 16) {
+            echo "You can buy specs!";
+        }
+        else if ($myage > 18) {
+            echo "You can buy specs and mugs!";
+        }
+        else if ($myage > 21) {
+            echo "You can buy specs, mugs and sausage rolls!";
+        }
         ?>
     </p>
 </body>
