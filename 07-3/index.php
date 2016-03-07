7158905170c342f4d4b353a4988950ea6bc8c239
A <A>
2016-03-07 10:11:57 +0000

    7-3-2016

--- 07-3/index.php
+++ 07-3/index.php
@@ -18,17 +18,17 @@
             echo "You can buy specs, mugs and sausage rolls!";
         }
 
-        $wantedgood = "mugs";
+        $wantedgood = "specs";
 
         switch ($wantedgood) {
             case "specs":
-                echo "You have to be at least 16 to buy mugs";
+                echo "You have to be at least 16 to buy specs";
                 break;
             case "mugs":
                 echo "You have to be at least 18 to buy mugs";
                 break;
             case "sausage rolls":
-                echo "You have to be at least 21 to buy mugs";
+                echo "You have to be at least 21 to buy sausage rolls";
                 break;
             default:
                 echo "You cannot buy anything";
