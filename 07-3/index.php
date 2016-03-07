e16b946cc333caaea944f9668bdfb5010ba704b4
A <A>
2016-03-07 10:11:14 +0000

    7-3-2016

--- 07-3/index.php
+++ 07-3/index.php
@@ -17,6 +17,22 @@
         else if ($myage > 21) {
             echo "You can buy specs, mugs and sausage rolls!";
         }
+
+        $wantedgood = "mugs";
+
+        switch ($wantedgood) {
+            case "specs":
+                echo "You have to be at least 16 to buy mugs";
+                break;
+            case "mugs":
+                echo "You have to be at least 18 to buy mugs";
+                break;
+            case "sausage rolls":
+                echo "You have to be at least 21 to buy mugs";
+                break;
+            default:
+                echo "You cannot buy anything";
+        }
         ?>
     </p>
 </body>
