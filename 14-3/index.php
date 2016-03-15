98458909da5d0ff5fa140b8bb1b08026812d0a3e
A <A>
2016-03-15 09:33:32 +0000

    15-3-2016

--- 14-3/index.php
+++ 14-3/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta> charset="utf-8">
+    <meta charset="utf-8">
     <title>PHP Login Form without Session</title>
     <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
