cfd28130264846e753824cac404be5bb93097cad
A <A>
2016-03-15 10:14:06 +0000

    15-3-2016

---
+++ 15-3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Marvel</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+    <a href="allmovies.php">Display all movies</a>
+    <a href="marvelmovies.php">Display movies created by Marvel Studios</a>
+    <a href="2010movies.php">Display movies created after 2010</a>
+    <a href="xmenmovies.php">Display X-Men Films</a>
+
+
+</body>
+</html>
+
+
+
---
+++ 15-3/marvelmovies.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510996
+ * Date: 15/03/2016
+ * Time: 10:12
+ */
