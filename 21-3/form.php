260b248e42cad196667f6b10e37bbf849512f50e
A <A>
2016-03-21 10:14:11 +0000

    21-3-2016

---
+++ 21-3/form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Selector</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+<form action="formResponse.php" method="post">
+    <label>Surname</label><input type=“text” name=“surname”>
+    <label>Surname</label><input type=“text” name=“surname”>
+    <label>Gender</label><input type=“text” name=“gender”>
+    <label>Main Superhero Power</label><input type=“text” name=“power”>
+    <input type="submit" value="Submit">
+</form>
+
+
+</body>
+</html>
+
+
+
