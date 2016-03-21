978cf58c081513588bc7e2902cca199b1286ef22
A <A>
2016-03-21 10:29:43 +0000

    21-3-2016

--- 21-3/form.php
+++ 21-3/form.php
@@ -8,10 +8,10 @@
 
 <body>
 <form action="response.php" method="post">
-    <label>Surname</label><input type=“text” name=“surname”><br><br>
-    <label>Name</label><input type=“text” name=“name”><br><br>
-    <label>Gender</label><input type=“text” name=“gender”><br><br>
-    <label>Main Superhero Power</label><input type=“text” name=“power”>
+    <label>Surname </label><input type=“text” name=“surname”><br><br>
+    <label>Name </label><input type=“text” name=“name”><br><br>
+    <label>Gender </label><input type=“text” name=“gender”><br><br>
+    <label>Main Superhero Power </label><input type=“text” name=“power”>
     <input type="submit" value="Submit">
 </form>
 
