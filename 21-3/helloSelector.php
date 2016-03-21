bd80be7280bf508d06a71fbd2f2cf0323b059f24
A <A>
2016-03-21 10:05:29 +0000

    21-3-2016

--- 21-3/helloSelector.php
+++ 21-3/helloSelector.php
@@ -8,8 +8,8 @@
 
 <body>
 <p>Where would you like to say hello?</p>
-<a href="helloPrinter.php?planet=earth">Earth</a>
-<a href="helloPrinter.php?planet=mars">Mars</a>
+<a href="helloPrinter.php?planet=earth">Earth</a><br>
+<a href="helloPrinter.php?planet=mars">Mars</a><br>
 <a href="helloPrinter.php?planet=uranus">Uranus</a>
 
 </body>
