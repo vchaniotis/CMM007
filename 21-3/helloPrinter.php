4b9ccb8b2c5ec5cee467a38125a7d64f3eadf37b
A <A>
2016-03-21 09:49:07 +0000

    21-3-2016

---
+++ 21-3/helloPrinter.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510996
+ * Date: 21/03/2016
+ * Time: 09:39
+ */
---
+++ 21-3/helloSelector.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Selector</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+<p>Where would you like to say hello?</p>
+<a href=""
+<?php
+
+
+?>
+</body>
+</html>
+
+
+
