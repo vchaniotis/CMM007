8566ed7a1e0d41bbfdc458d36e5e444f6bfe97e8
A <A>
2016-03-22 10:33:50 +0000

    22-3-2016

---
+++ 22-3/insertsuperhero.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510996
+ * Date: 22/03/2016
+ * Time: 10:01
+ */
+$surname = $_POST["surnmane"];
+$name = $_POST["name"];
+$gender = $_POST["gender"];
+$power = $_POST["power"];
+
+
+?>
+
+
+</body>
+</html>
