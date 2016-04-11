9b8c2bcfb4efe448e3e43a72be73826f7d15d295
A <A>
2016-04-11 10:36:37 +0100

    11-4-2016

--- 11-4/loggedin.php
+++ 11-4/loggedin.php
@@ -10,14 +10,16 @@ $accesslevel = $_COOKIE['access_level_cookie'];
 
 displayAccessLevelInformation($accesslevel);
 
-function displayAccessLevelInformation($accesslevel)
+function displayAccessLevelInformation($accessLevel)
 {
-    if ($accessLevel == "standarduser") {
-        echo "<p style = \"background-color: lightgreen\">You are currently logged in as a standard user</p>;
+    if ($accessLevel == "standarduser")
+    {
+        echo "<p style = \"background-color: lightgreen\">You are currently logged in as a standard user</p>";
     }
-    elseif ($accessLevel == "root") {
-        echo "<p style = \"background-color: red\">You are currently logged in as a root user</p>;
-        echo "<p style = \"background-color: red\">You now have access to additional administrative features</p>;
+    elseif ($accessLevel == "root")
+    {
+        echo "<p style = \"background-color: red\">You are currently logged in as a root user</p>";
+        echo "<p style = \"background-color: red\">You now have access to additional administrative features</p>";
     }
 }
 
