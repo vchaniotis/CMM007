f3a6d7cf1d2f695673c7ddd6f77037b4643ca6a1
A <A>
2016-04-11 10:23:35 +0100

    11-4-2016

--- 11-4/checklogin.php
+++ 11-4/checklogin.php
@@ -9,7 +9,7 @@
 $username = $_POST["name"];
 $password = $_POST["password"];
 
-if ($username == "username" && password == "password")
+if ($username == "username" && $password == "password")
 {
     setcookie('access_level_cookie', 'standarduser');
 }
