4f6c93575d39bf8e32067cdfb361039c1ad5a8d0
A <A>
2016-04-12 10:13:16 +0100

    12-4-2016

--- 12-4/printPOST.php
+++ 12-4/printPOST.php
@@ -5,8 +5,10 @@
  * Date: 12/04/2016
  * Time: 09:57
  */
+$title = $_POST["title"];
+
 echo "<article>
-        <h1></h1>
+        <h1>$title</h1>
         <p></p>
         <strong></strong>
       </article>";
