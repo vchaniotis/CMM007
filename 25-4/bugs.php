da4d4c4a93c65f334f932aa22fee37343934f003
A <A>
2016-04-25 09:23:50 +0100

    25-4-2016

---
+++ 25-4/bugs.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Index</title>
+</head>
+<body>
+<div id="wrap">
+    <header>
+        <div id="header">
+            <img src="logo.png">
+            <div id="section">
+                <h1>BugTracker</h1>
+                <p id="subtitle">Keeping track of all the pesky little bugs</p>
+            </div>
+        </div>
+    </header>
+    <div id="mainbody">
+        <div id="navigation">
+            <ul>
+                <li><a href="bugs.html">All Bug Items</a></li><br>
+                <li><a href="bugs.html">Android Bugs</a></li><br>
+                <li><a href="bugs.html">iOS Bugs</a></li><br>
+                <li><a href="bugs.html">Windows Bugs</a></li><br>
+                <li><a href="bugs.html">Insert Bugs</a></li><br>
+            </ul>
+        </div>
+        <content>
+            <div id="form">
+                <form>
+                    <div id="name">Bug Name <input type="text" name="name" id="boxname"><br></div>
+                    <div id="summary">Bug Summary <input type="text" name="summary" id="boxsummary"><br></div>
+                    <div id="category">Bug Category <input type="text" name="category" id="boxcategory"><br></div>
+                    <input type="submit" value="Submit" id="button">
+                </form>
+            </div>
+        </content>
+    </div>
+    <footer>Designed by Chaniotis Vasileios 2016</footer>
+</div>
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510996
+ * Date: 25/04/2016
+ * Time: 09:23
+ */
---
+++ 25-4/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Index</title>
+</head>
+<body>
+<div id="wrap">
+    <header>
+        <div id="header">
+            <img src="logo.png">
+            <div id="section">
+                <h1>BugTracker</h1>
+                <p id="subtitle">Keeping track of all the pesky little bugs</p>
+            </div>
+        </div>
+    </header>
+    <div id="mainbody">
+        <div id="navigation">
+            <ul>
+                <li><a href="bugs.php">All Bug Items</a></li><br>
+                <li><a href="bugs.php">Android Bugs</a></li><br>
+                <li><a href="bugs.php">iOS Bugs</a></li><br>
+                <li><a href="bugs.php">Windows Bugs</a></li><br>
+                <li><a href="bugs.php">Insert Bugs</a></li><br>
+            </ul>
+        </div>
+        <content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </content>
+    </div>
+    <footer>Designed by Chaniotis Vasileios 2016</footer>
+</div>
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510996
+ * Date: 25/04/2016
+ * Time: 09:22
+ */
